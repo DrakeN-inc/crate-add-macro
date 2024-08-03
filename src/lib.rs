dfc4db86280b1ccfc48983a95023f406f701679f
Bulat Sharipov <draken-inc@yandex.ru>
2024-08-03 07:29:32 +0500

    Little changes..

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,29 @@ pub mod collections;
 pub mod io;
 
 
-// pub use add_macro_impl_display::Display;
-// pub use add_macro_impl_fromstr::FromStr;
-// pub use add_macro_impl_error::Error;
+/* TODO:
+    use add_macro_impl_display::Display;
+    use add_macro_impl_error::Error;
+    use add_macro_impl_fromstr::FromStr;
+    use add_macro_impl_from::From;
+    use add_macro_impl_into::Into;
+
+    #[derive(Debug, Display, Error, From)]
+    #[from(std::io::Error = "Self::Io(v)")]
+    enum Error {
+        #[display("{}")]
+        Io(std::io::Error),
+
+        #[display("{}")]
+        Regex(#[from] regex::Error),
+
+        #[display("Wrong a login or password data!")]
+        WrongLoginData,
+    }
+
+    fn main() {
+        let err = Error::WrongLoginData;
+
+        assert_eq!(err.source(), "Wrong a login or password data!");
+    }
+*/
